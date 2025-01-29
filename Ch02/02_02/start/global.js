1ed3f6b9b6cb6f4bf2949d8934e45359b4b980b6
unbent <daniel.polan@hotmail.com>
2025-01-29 17:32:31 -0500

    Using the require function

--- Ch02/02_02/start/global.js
+++ Ch02/02_02/start/global.js
@@ -1,3 +1,24 @@
-let hello = "Hello World from Node.js";
-let justNode = hello.slice(17);
-console.log(`Who let the ${justNode} out?`);
+const path = require("path"); //Note: Have to do this because Node.js doesn't automatically share modules across files
+
+console.log(__dirname);
+console.log(`The filename is ${path.basename(__filename)}`);
+
+for (let key in global) {
+    console.log(key);
+}
+
+// These are the list of keys:
+    // global
+    // clearImmediate
+    // setImmediate
+    // clearInterval
+    // clearTimeout
+    // setInterval
+    // setTimeout
+    // queueMicrotask
+    // structuredClone
+    // atob
+    // btoa
+    // performance
+    // fetch
+    // crypto
