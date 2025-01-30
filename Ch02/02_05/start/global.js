298e021b74eddac82c904980b78826bc339f0aac
unbent <daniel.polan@hotmail.com>
2025-01-30 14:23:49 -0500

    Using Standard Output

--- Ch02/02_05/start/global.js
+++ Ch02/02_05/start/global.js
@@ -1,19 +1,36 @@
-process.stdout.write("Hello  \n \n");
+process.stdout.write("Hello  \n \n")
 
-const questions = [
-  "What is your name?",
-  "What would you rather be doing?",
-  "What is your preferred programming language"
-];
-const answers = [];
-
-function ask(i) {
-  process.stdout.write(`\n\n\n ${questions[i]}`);
-  process.stdout.write(` > `);
+const questionsAndAnswers = {
+  'Do you wear wigs?': '',
+  'Have you worn wigs?': '',
+  'When will you wear wigs?': '',
 }
 
-process.stdin.on("data", function (data) {
-  process.stdout.write(data.toString().trim());
+// I wanted a more iterative approach to this problem,
+// and a way to keep the questions and answers together in case
+// we wanted to use an Enum later to search up a specific answer.
+
+const askQuestion = (questions) => {
+  if (questions.length === 0) {
+    process.exit();
+  }
+
+  const [question] = questions[0];
+
+  process.stdout.write(`\n\n\n ${question} \n >`);
+
+  process.stdin.once("data", function (data) {
+    questionsAndAnswers[question] = data.toString().trim();
+
+    askQuestion(questions.slice(1));
+  });
+};
+
+process.on("exit", function() {
+  process.stdout.write("\n\n\nAll questions answered. Goodbye!\n\n");
+  Object.entries(questionsAndAnswers).forEach(([question, answer]) => {
+    process.stdout.write(`${question} ${answer}\n`);
+  });
 });
 
-ask(answers.length);
+askQuestion(Object.entries(questionsAndAnswers));
