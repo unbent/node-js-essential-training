3f8d0a02c4489025972ef68cff47c5e3fcbc8843
unbent <daniel.polan@hotmail.com>
2025-01-30 14:40:20 -0500

    Reporting Progress With SetInterval

--- Ch02/02_08/start/global.js
+++ Ch02/02_08/start/global.js
@@ -1,6 +1,11 @@
 const waitTime = 3000;
 console.log(`setting a ${waitTime / 1000} second delay`);
-const timerFinished = () => console.log("done");
+const timerFinished = () => {
+  clearInterval(interval)
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write("\ncomplete 100%\n")
+};
 
 setTimeout(timerFinished, waitTime);
 
@@ -8,6 +13,10 @@ const waitInterval = 500;
 let currentTime = 0;
 const incTime = () => {
   currentTime += waitInterval;
+  const p = Math.floor((currentTime / waitTime) * 100);
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(`waiting... ${p}%`)
 };
 
 const interval = setInterval(incTime, waitInterval);
