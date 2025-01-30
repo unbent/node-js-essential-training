c511f0dd0a808ea7144233f3bb2529c0992fcabe
unbent <daniel.polan@hotmail.com>
2025-01-30 14:33:42 -0500

    Incorporating SetInterval

--- Ch02/02_07/start/global.js
+++ Ch02/02_07/start/global.js
@@ -1,4 +1,17 @@
 const waitTime = 3000;
 console.log(`setting a ${waitTime / 1000} second delay`);
-const timerFinished = () => console.log("done");
+const timerFinished = () => {
+    clearInterval(interval)
+    console.log("done")
+};
 setTimeout(timerFinished, waitTime);
+
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval;
+    console.log(`waiting ${currentTime / 1000} seconds.`);
+};
+
+const interval = setInterval(incTime, waitInterval);
